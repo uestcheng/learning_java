9be80c2904620538032966465a1b646ef11d5376
Tom Heng <uestcheng@gmail.com>
2016-07-06 22:54:46 +0800

    complete package test

---
+++ src/EmployeeTest/EmployeeForPT.java
@@ -0,0 +1,33 @@
+package EmployeeTest;
+
+import java.time.*;
+
+public class EmployeeForPT{
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+
+    public EmployeeForPT(String name, double salary, int year, int month, int day){
+        this.name = name;
+        this.salary = salary;
+        hireDay = LocalDate.of(year, month, day);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public LocalDate getHireDay(){
+        return hireDay;
+    }
+
+    public void raiseSalary(double byPercent){
+        double raise = salary * byPercent / 100;
+        salary = salary + raise;
+    }
+}
+
--- src/PackageTest/PackageTest.java
+++ src/PackageTest/PackageTest.java
@@ -5,6 +5,11 @@ import static java.lang.System.*;
 
 public class PackageTest{
     public static void main(String[] args){
-        Employee
+        EmployeeForPT harry = new EmployeeForPT("Harry Hacker", 50000, 1989, 10, 1);
+
+
+        harry.raiseSalary(5);
+
+        out.println("name="+harry.getName()+",salary="+harry.getSalary());
     }
 }
