9d275af984442a943c210bf3a7be668dd061d930
Tom Heng <uestcheng@gmail.com>
2016-07-24 23:39:54 +0800

    Complete ReflectionTest

--- src/reflection/ReflectionTest.java
+++ src/reflection/ReflectionTest.java
@@ -46,7 +46,62 @@ public class ReflectionTest{
         for(Constructor c: constructors){
             String name = c. getName();
             System.out.print(" ");
+            String modifiers = Modifier.toString(c.getModifiers());
+            if(modifiers.length() > 0){
+                System.out.print(modifiers + " ");
+            }
+            System.out.print(name + "(");
+
+            Class[] paramTypes = c.getParameterTypes();
+            for(int j = 0; j < paramTypes.length; j++){
+                if(j > 0){
+                    System.out.print(", ");
+                }
+                System.out.print(paramTypes[j].getName());
+            }
+            System.out.println(");");
         }
 
     }
+
+    public static void printMethods(Class cl){
+        Method[] methods = cl.getDeclaredMethods();
+        for(Method m : methods){
+            Class retType = m.getReturnType();
+            String name = m.getName();
+
+            System.out.print(" ");
+
+            String modifiers = Modifier.toString(m.getModifiers());
+            if(modifiers.length() > 0){
+                System.out.print(modifiers + " ");
+            }
+            System.out.print(retType.getName() + " " + name + "(");
+
+            Class[] paramType = m.getParameterTypes();
+            for(int j = 0; j < paramType.length; j++){
+                if(j > 0){
+                    System.out.print(",");
+                }
+                System.out.print(paramType[j].getName());
+            }
+            System.out.print(");");
+        }
+    }
+
+    public static void printFields(Class cl){
+        Field[] fields = cl.getDeclaredFields();
+
+        for(Field f:fields){
+            Class type = f.getType();
+            String name = f.getName();
+            System.out.print(" ");
+            String modifiers = Modifier.toString(f.getModifiers());
+            if(modifiers.length() > 0){
+                System.out.print(modifiers + " ");
+            }
+            System.out.println(type.getName()+ " " + name + ";");
+        }
+    }
 }
+
